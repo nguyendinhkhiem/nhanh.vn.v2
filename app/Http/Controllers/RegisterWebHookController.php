e224e795017027e1f976c03cd4a1b7d4245b082c
Khiêm Nguyễn <nguyendinhkhiem98@gmail.com>
2019-09-30 22:47:00 +0700

    update

--- app/Http/Controllers/RegisterWebHookController.php
+++ app/Http/Controllers/RegisterWebHookController.php
@@ -16,8 +16,6 @@ class RegisterWebHookController extends Controller
     {
         $result = $this->webhook_url();
 
-            var_dump($result);
-            die();
 
         $url = route('listen-webhook');
         if ($url !== $result->data) {
@@ -27,6 +25,8 @@ class RegisterWebHookController extends Controller
         $data = ['url' => route('listen-webhook')];
         $add  = $this->curl("https://services.giaohangtietkiem.vn/services/webhook/add", $data);
 
+            var_dump($add);
+            die();
         if ($add->success) {
             $resultAddWebHook = $this->webhook_url();
         }
