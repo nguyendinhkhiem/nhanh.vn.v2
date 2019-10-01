bb8a031b6f03d9a06510ab5b7497f3301fd1ee4b
Khiêm Nguyễn <nguyendinhkhiem98@gmail.com>
2019-10-01 21:33:18 +0700

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -256,9 +256,8 @@ class OrderController extends Controller
 
                 $dequyKetQua = [];
                 if ($response->data->totalPages > 1) {
-                    $dequyKetQua = $this->searchDeQuy($response->data->totalPages, $response->data->page, $_GET['type'], $_GET['value']);
-                    var_dump(json_decode($dequyKetQua));
-                    die();
+                    $dequyKetQuaBak = $this->searchDeQuy($response->data->totalPages, $response->data->page, $_GET['type'], $_GET['value']);
+                    array_push($dequyKetQua, $dequyKetQuaBak)
                 }
                 if ($dequyKetQua) {
                     $sumResponse = array_merge($responseFrontEnd, json_decode($dequyKetQua));
