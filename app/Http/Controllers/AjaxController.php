9b41dbaa88fe6c28e6279ea964921c86342dcbc2
vagrant <vagrant@homestead>
2019-10-17 04:37:02 +0000

    Sua Ajax Controller

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -51,30 +51,23 @@ class AjaxController extends Controller
 
     public function detailOrderBySearch($id)
     {
-        $order = Order::where(function ($query) use ($id) {
+        $order = Order::where(function ($query) use ($id) {     
             $query->orWhere('label_GHTK', 'like', "%$id%");
-            $query->orWhere('customerName', 'like', "%$id%");
+            // $query->orWhere('customerName', 'like', "%$id%");
             $query->orWhere('customerMobile', 'like', "%$id%");
             $query->orWhere('id_nhanhvn', 'like', "%$id%");
-        })->get();
+        })
+        ->skip(0)
+        ->take(5)
+        ->get();
         // dd($order);exit();
 
         if(!empty($order)){
-            $order_id = $order->id;
-            $products = Product::where('order_id', $order_id)->get();
-            $causes = Cause::where('order_id', $order_id)->orderBy('created_at', 'desc')->get();
-            $data = [
-                'order'    => $order,
-                'products' => $products,
-                'causes' => $causes
-            ];
-            return $data;
+            return $order;
         }else{
             return 'Đơn hàng chưa đăng lên GHTK!';
         }
     }
-
-
 
     
 }
